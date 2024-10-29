0f39443882bc13dcad40c743d47fa93e190ee54c
Konstantin Lukas <konstantin_lukas@web.de>
2024-10-29 09:47:56 +0100

    add punctuation detection to inline link

--- components/shared/InlineLink.tsx
+++ components/shared/InlineLink.tsx
@@ -1,22 +1,31 @@
+"use client";
+
 import Link from "next/link";
-import React, { type ReactNode } from "react";
+import React, { type ReactNode, useEffect, useRef, useState } from "react";
 
 export default function H1({ children, href, target }: {
     children: ReactNode,
     href: string,
     target?: string
 }) {
+    const ref = useRef<HTMLAnchorElement>(null);
+    const [space, setSpace] = useState("");
+    useEffect(() => {
+        const nextChar = ref.current?.nextSibling?.textContent?.[0];
+        if (nextChar && ![",", ".", "?", "!", ";", ":"].includes(nextChar)) setSpace(" ");
+    }, []);
     return (
         <>
             {" "}
             <Link
+                ref={ref}
                 href={href}
                 target={target}
                 className="inline-link bg-gradient-to-b from-hu-blue-secondary to-hu-blue-secondary font-medium text-hu-blue-secondary"
             >
                 {children}
             </Link>
-            {" "}
+            {space}
         </>
     );
 }
